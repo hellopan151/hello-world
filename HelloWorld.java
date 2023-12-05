51dbcdac5f6f56fc3534678a59c8d4a5d6817923
hellopan151 <hellopan@gmail.com>
2023-12-05 16:17:32 +0800

    Add second class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main( String[] arsg ) {
 		System.out.println( "Hello World!" );
+		System.out.println( "First " + new Second().getString(" this is from First" ) );
 	}
 }
---
+++ Second.java
@@ -0,0 +1,5 @@
+public class Second {
+	String getString( String inputString ) {
+		return "Second class " + inputString;
+	}
+}
